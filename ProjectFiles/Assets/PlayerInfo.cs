5142d6388f84e4d537572e2c1aa423f4d7159c01
xiyuzhou <xyz2333@outlook.com>
2023-11-02 17:48:49 -0700

    Upload files

---
+++ ProjectFiles/Assets/PlayerInfo.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using TMPro;
+using PlayFab.ClientModels;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [HideInInspector]
+    public PlayerProfileModel profile;
+    public TextMeshProUGUI LoginMessage;
+
+    public static PlayerInfo instance;
+    void Awake() { instance = this; }
+    public void OnLoggedIn()
+    {
+        GetPlayerProfileRequest getProfileRequest = new GetPlayerProfileRequest
+        {
+            PlayFabId = LoginRegister.instance.playFabId,
+            ProfileConstraints = new PlayerProfileViewConstraints
+            {
+                ShowDisplayName = true
+            }
+        };
+        PlayFabClientAPI.GetPlayerProfile(getProfileRequest,
+         result =>
+         {
+             profile = result.PlayerProfile;
+             Debug.Log("Loaded in player: " + profile.DisplayName);
+             LoginMessage.text = "Logged in as: " + result.PlayerProfile.DisplayName + '\n' + "PlayerID:" + result.PlayerProfile.PlayerId;
+         },
+         error => Debug.Log(error.ErrorMessage)
+        );
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
